050c3142492bdd8befab3ccd5dd2e23c4afeebad
John Sirois <john.sirois@gmail.com>
2025-11-29 07:38:58 -0600

    Convert find-links repo to simple-index.

--- packaging/build-wheels.sh
+++ packaging/build-wheels.sh
@@ -12,7 +12,7 @@ for arch in amd64 arm64; do
     docker run \
         --rm \
         --platform linux/${arch} \
-        -v ${ROOT}/packaging/find-links:/dist \
+        -v "${ROOT}/packaging/simple-index/psutil:/dist" \
         --env PYTHONHASHSEED=0 \
         --env SOURCE_DATE_EPOCH=315532800 \
             python:${python_version}-alpine sh -c '
@@ -22,6 +22,33 @@ for arch in amd64 arm64; do
         .venv/bin/auditwheel repair dist-temp/*.whl && \
         mv dist-temp/*.whl dist/
     ' && \
-    sudo chown -R ${USER}:${USER} ${ROOT}/packaging/find-links
+    sudo chown -R "${USER}:${USER}" "${ROOT}/packaging/simple-index/psutil"
 done
 
+cat <<EOF > "${ROOT}/packaging/simple-index/psutil/index.html"
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>psutil wheels</title>
+    </head>
+    <body>
+        <h1>psutil wheels</h1>
+EOF
+
+for whl_path in ${ROOT}/packaging/simple-index/psutil/*.whl; do
+    whl="$(basename "${whl_path}")"
+    sha256="$(sha256sum "${whl_path}" | cut -d' ' -f1)"
+
+    unzip -qc "${whl_path}" "$(echo "${whl}" | cut -d- -f1-2).dist-info/METADATA" > \
+        "${ROOT}/packaging/simple-index/psutil/${whl}.metadata"
+    metadata_sha256="$(sha256sum "${ROOT}/packaging/simple-index/psutil/${whl}.metadata" | cut -d' ' -f1)"
+
+    cat <<EOF >> "${ROOT}/packaging/simple-index/psutil/index.html"
+        <a href="${whl}#sha256=${sha256}" data-core-metadata="sha256=${metadata_sha256}">${whl}</a>
+EOF
+done
+
+cat <<EOF >> "${ROOT}/packaging/simple-index/psutil/index.html"
+    </body>
+</html>
+EOF
